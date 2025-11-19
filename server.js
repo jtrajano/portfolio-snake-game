f31d8121fdee49559bbc62dc9d951e72595132f3
jtrajano <jlt212000@yahoo.com>
2025-11-19 10:59:17 +0800

    fix ci cd deployment 2

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
