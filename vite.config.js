3678fa101bac822d0dbcd97f1804236f3d83ed93
jtrajano <jlt212000@yahoo.com>
2025-11-19 11:15:07 +0800

    fix buid error

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
