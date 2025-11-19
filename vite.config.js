f69d6c3b03f69a3d5d777dc6727f8ca359c4eeb0
jtrajano <jlt212000@yahoo.com>
2025-11-19 10:25:35 +0800

    fix the ci cd build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-web-config',
+      closeBundle() {
+        copyFileSync('web.config', 'dist/web.config')
+      }
+    }
+  ],
 })
